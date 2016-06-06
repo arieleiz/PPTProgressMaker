844da2b30c14618dd33ddc872aae6cbd389112ae
Ariel Eizenberg <ariel.eizenberg@gmail.com>
2016-06-05 21:20:08 -0400

    Missing file

---
+++ ToCStyle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTProgressMaker
+{
+    public class ToCStyle
+    {
+        public ToCStyle()
+        {
+            Type = Types.Horizontal;
+            Style = Styles.Gradient;
+            RTL = false;
+            FirstSlide = false;
+        }
+
+        public enum Types { Horizontal, Vertical };
+        public enum Styles { Solid, Gradient };
+
+        public Types Type { get; set; }
+        public Styles Style { get; set; }
+        public bool RTL { get; set;  }
+        public bool FirstSlide { get; set; }
+        public bool SlideNumbers { get; set; }
+        public bool IgnoreLastSection { get; set;  }
+    }
+}
